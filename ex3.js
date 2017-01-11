6d07a4a50788f71779feabcabc3298f5dd8e0fc9
bryaan.ly <bryaan.ly@gmail.com>
2017-01-11 05:01:14 -0500

    first commit

---
+++ ex3.js
@@ -0,0 +1,16 @@
+function rentrez(){
+  var date;
+  date =prompt("Rentrez votre date de naissance.");
+
+  if (date<=31){
+    
+alert('Format de date valide');
+}
+else
+  alert('Veuillez rentrer une date valide !');
+}
+rentrez();
+
+
+
+  
